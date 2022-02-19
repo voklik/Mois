1ec07e27cf06109444fa17b8419146a8d2617859
voklik <voklik@spoluzaci.cz>
2022-02-19 18:44:47 +0100

    Init Commits

---
+++ src/test/java/mois/projekt/Uhk1ApplicationTests.java
@@ -0,0 +1,13 @@
+package mois.projekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uhk1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
