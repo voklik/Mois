db9aa0614ffc17b3eee84c151221de6f14279158
Kristyna Hnizdilova <Hnizdilova2@seznam.cz>
2022-03-24 23:30:01 +0200

    Update translations + init mui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,125 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import GlobalStyles from '@mui/material/GlobalStyles';
+import Container from '@mui/material/Container';
+import { useTranslation } from 'react-i18next';
 
-function App() {
+const footers = [
+  {
+    title: 'Trave Agency',
+    description: ['Team', 'History', 'Contact us', 'Locations'],
+  },
+  {
+    title: 'Features',
+    description: [
+      'Cool stuff',
+      'Random feature',
+      'Team feature',
+      'Developer stuff',
+      'Another one',
+    ],
+  },
+  {
+    title: 'Resources',
+    description: ['Resource', 'Resource name', 'Another resource', 'Final resource'],
+  },
+  {
+    title: 'Legal',
+    description: ['Privacy policy', 'Terms of use'],
+  },
+];
+
+function Welcome()
+{
+    const {t, i18n} = useTranslation('common');
+    return <h1>{t('welcome.title', {title:'Travel Agency'})}</h1>
+}
+
+function LanguageBar(){
+  const {t, i18n} = useTranslation('common');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+  <Container>
+  <Button onClick={() => i18n.changeLanguage('cs')}>cs</Button>
+  <Button onClick={() => i18n.changeLanguage('en')}>en</Button>
+  </Container>
+  )
+}
+
+function PricingContent() {
+  return (
+      <React.Fragment>
+        <GlobalStyles styles={{ ul: { margin: 0, padding: 0, listStyle: 'none' } }} />
+        <CssBaseline />
+        <AppBar
+            position="static"
+            color="default"
+            elevation={0}
+            sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Toolbar sx={{ flexWrap: 'wrap' }}>
+            <Typography variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
+              Company name
+            </Typography>
+            <nav>
+            <Button href="#" variant="outlined" sx={{ my: 1, mx: 1.5 }}>
+              Login
+            </Button>
+            <LanguageBar></LanguageBar>
+            </nav>
+          </Toolbar>
+        </AppBar>
+        {/* Hero unit */}
+        <Container disableGutters maxWidth="sm" component="main" sx={{ pt: 8, pb: 6 }}>
+          <Typography
+              component="h1"
+              variant="h4"
+              align="center"
+              color="text.primary"
+              gutterBottom
+          >
+           <Welcome></Welcome>
+          </Typography>
+          </Container>
+        {/* Footer */}
+        <Container
+            maxWidth="md"
+            component="footer"
+            sx={{
+              borderTop: (theme) => `1px solid ${theme.palette.divider}`,
+              mt: 8,
+              py: [3, 6],
+            }}
+        >
+          <Grid container spacing={4} justifyContent="space-evenly">
+            {footers.map((footer) => (
+                <Grid item xs={6} sm={3} key={footer.title}>
+                  <Typography variant="h6" color="text.primary" gutterBottom>
+                    {footer.title}
+                  </Typography>
+                  <ul>
+                    {footer.description.map((item) => (
+                        <li key={item}>
+                          <Link href="#" variant="subtitle1" color="text.secondary">
+                            {item}
+                          </Link>
+                        </li>
+                    ))}
+                  </ul>
+                </Grid>
+            ))}
+          </Grid>
+        </Container>
+        {/* End footer */}
+      </React.Fragment>
   );
 }
 
-export default App;
+export default function Pricing() {
+  return <PricingContent />;
+}
--- src/index.js
+++ src/index.js
@@ -3,10 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {I18nextProvider} from "react-i18next";
+import i18next from "i18next";
+import common_cs from "./assets/translations/cs/common.json";
+import common_en from "./assets/translations/en/common.json";
+
+i18next.init({
+    interpolation: { escapeValue: false },
+    lng: 'cs',                              // language to use
+    resources: {
+        en: {
+            common: common_en          
+        },
+        cs: {
+            common: common_cs
+        },
+    },
+});
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+        <I18nextProvider i18n={i18next}>
+            <App/>
+        </I18nextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
